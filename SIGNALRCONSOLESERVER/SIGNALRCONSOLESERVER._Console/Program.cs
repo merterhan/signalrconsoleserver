c34c9767bd1e28a8648a0353e363335bab1f4837
mert.erhan <mertcagriberkerhan@gmail.com>
2018-07-24 09:52:06 +0300

    -somechanges

--- SIGNALRCONSOLESERVER/SIGNALRCONSOLESERVER._Console/Program.cs
+++ SIGNALRCONSOLESERVER/SIGNALRCONSOLESERVER._Console/Program.cs
@@ -1,42 +1,110 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.Owin.Cors;
 using Microsoft.Owin.Hosting;
-using Owin;
+using Newtonsoft.Json;
+using SIGNALRCONSOLESERVER._Console.Model;
 using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using TableDependency;
+using TableDependency.EventArgs;
+using TableDependency.SqlClient;
 
 namespace SIGNALRCONSOLESERVER._Console
 {
     class Program
     {
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+        public static string GetLocalFQDN()
+        {
+            var props = IPGlobalProperties.GetIPGlobalProperties();
+            return props.HostName + (string.IsNullOrWhiteSpace(props.DomainName) ? "" : "." + props.DomainName);
+        }
+        private static string _con = "data source=localhost;initial catalog=Test;user id=sa;password=123456;MultipleActiveResultSets=True;App=EntityFramework";
+
         static void Main(string[] args)
         {
             // This will *ONLY* bind to localhost, if you want to bind to all addresses
             // use http://*:8080 to bind to all addresses. 
             // See http://msdn.microsoft.com/library/system.net.httplistener.aspx 
             // for more information.
-            string url = "http://localhost:8080";
+            string url = "http://*:8081";
+            ////when server is remote run the command below on cmd administrator
+            ////netsh http add urlacl url=http://*:8081/ user=Everyone
+
+            int startin = 60 - DateTime.Now.Second;
+            var t = new System.Threading.Timer(o => sendALot(),
+                 null, startin * 1000, 3000);
+
             using (WebApp.Start(url))
             {
                 Console.WriteLine("Server running on {0}", url);
                 Console.ReadLine();
             }
+
+            ////SQLTABLEDEPENDENCY
+            //using (var dep = new SqlTableDependency<Alarm>(_con, tableName: "Alarm"))
+            //{
+            //    dep.OnChanged += TableChanged;
+            //    dep.Start();
+
+            //    Console.WriteLine("Press a key to exit");
+            //    Console.ReadKey();
+
+            //    dep.Stop();
+            //}
+            ////end SQLTABLEDEPENDENCY
         }
-    }
-    class Startup
-    {
-        public void Configuration(IAppBuilder app)
+
+        //public static void TableChanged(object sender, RecordChangedEventArgs<Alarm> e)
+        //{
+        //    var changedEntity = e.Entity;
+        //    //string result = JsonConvert.SerializeObject(changedEntity, Formatting.Indented);
+        //    //Console.WriteLine(result);
+        //    //Console.ReadLine();
+        //}
+
+        static void sendALot()
         {
-            app.UseCors(CorsOptions.AllowAll);
+            Random rnd = new Random();
+            var context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+            context.Clients.All.addMessage(rnd.Next(0, 6));
+            Console.WriteLine("Sended at: " + DateTime.Now.ToString());
 
-            //Sending detailed error messages to clients in production is not recommended for security reasons, but if you want to enable detailed error messages for troubleshooting purposes, use the following code on the server.
-            var hubConfiguration = new HubConfiguration();
-            hubConfiguration.EnableDetailedErrors = true;
+        }
 
-            app.MapSignalR(hubConfiguration);
+        public void InsertAlarm(Alarm model)
+        {
+            using (TestEntities db = new TestEntities())
+            {
+                try
+                {
+                    db.Alarm.Add(model);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("InsertAlarm() error: " + e.Message);
+                }
+            }
         }
     }
+
     [HubName("MyHub")]
     public class MyHub : Hub
     {
@@ -45,9 +113,9 @@ namespace SIGNALRCONSOLESERVER._Console
             return message;
         }
 
-        public void DoSomething(string param)
+        public void DoSomething(int? param)
         {
-            Clients.All.addMessage(param);
+            Clients.All.addMessage("0");
         }
 
         public override Task OnConnected()
---
+++ SIGNALRCONSOLESERVER/SIGNALRCONSOLESERVER._Console/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(SIGNALRCONSOLESERVER._Console.Startup))]
+
+namespace SIGNALRCONSOLESERVER._Console
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/signalr", map =>
+            {
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true,
+                };
+                map.UseCors(CorsOptions.AllowAll);
+                map.RunSignalR(hubConfiguration);
+            });
+        }
+    }
+}
